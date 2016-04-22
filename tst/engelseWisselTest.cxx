14635567ecd08962d2a397bff76eb7edf9f3ca3e
Zoev89 <eric.trein@gmail.com>
2016-04-22 20:39:35 +0200

    Add terug richting test voor engelsewissel

--- tst/engelseWisselTest.cxx
+++ tst/engelseWisselTest.cxx
@@ -54,6 +54,9 @@ TEST_F(engelseWisselTest, Constructie)
     InitBlok(4);
     InitBlok(5);
     InitWissel(0,ENGELSE_WISSEL,100 , 01);
+    //   4-> \          |
+    //   1 -> 2 -> 3    |
+    //         \-> 5    |
     objects->blok.InitBlok("1      B2  -1 134 0 3 2  100  100");
     objects->blok.InitBlok("2      B3  -1 134 0 3 2  100  100");
     objects->blok.InitBlok("3      B-1  -1 134 0 3 2  100  100");
@@ -69,54 +72,60 @@ TEST_F(engelseWisselTest, Constructie)
     EXPECT_EQ(0, baanInfo->BlokPointer[4].pVolgendBlok->BlokIONummer);
     EXPECT_EQ(&baanInfo->Blok[2], baanInfo->BlokPointer[4].pVolgendBlok->pBlok); // wissel blok moet naar blok 2 wijzen
     EXPECT_EQ(5, baanInfo->BlokPointer[4].pVolgendBlok->pVolgendBlok->BlokIONummer);
+    EXPECT_EQ(1, baanInfo->BlokPointer[2].pVorigBlok->BlokIONummer);
+    EXPECT_EQ(2, baanInfo->BlokPointer[3].pVorigBlok->BlokIONummer);
+    EXPECT_EQ(0, baanInfo->BlokPointer[5].pVorigBlok->BlokIONummer);
+    EXPECT_EQ(&baanInfo->Blok[2], baanInfo->BlokPointer[5].pVorigBlok->pBlok); // wissel blok moet naar blok 2 wijzen
+    EXPECT_EQ(4, baanInfo->BlokPointer[5].pVorigBlok->pVorigBlok->BlokIONummer);
+
     EXPECT_EQ(13,baanInfo->IOBits[0]->Stand);
 
     //
     // bedien de wissel in de andere stand en check de blokken
     //
     EXPECT_CALL(objects->baanMessage, Post(WM_WISSEL_DISPLAY, 0, 0, 0));
-    std::promise<void> done1;
-    {
-        EXPECT_CALL(mHardwareHoog,nieuwItem(_)).WillOnce(Invoke( [&done1] (const hardwareArray_t & data){
-            EXPECT_EQ(100,data.adres);
-            EXPECT_EQ(1+(3<<4),data.data); // get status
-            done1.set_value();
-            return 0;
-        }));
-    }
+    EXPECT_CALL(mHardwareHoog,nieuwItem(_)).WillOnce(Invoke( [] (const hardwareArray_t & data){
+        EXPECT_EQ(100,data.adres);
+        EXPECT_EQ(1+(3<<4),data.data); // get status
+        return 0;
+    }));
 
     EXPECT_EQ(0,baanInfo->IOBits[0]->Aanvraag(12));
     EXPECT_EQ(12,baanInfo->IOBits[0]->Stand);
-    EXPECT_EQ(std::future_status::ready, done1.get_future().wait_for(std::chrono::seconds(1)));
 
     EXPECT_EQ(2, baanInfo->BlokPointer[1].pVolgendBlok->BlokIONummer);
     EXPECT_EQ(5, baanInfo->BlokPointer[2].pVolgendBlok->BlokIONummer);
     EXPECT_EQ(0, baanInfo->BlokPointer[4].pVolgendBlok->BlokIONummer);
     EXPECT_EQ(&baanInfo->Blok[2], baanInfo->BlokPointer[4].pVolgendBlok->pBlok); // wissel blok moet naar blok 2 wijzen
     EXPECT_EQ(3, baanInfo->BlokPointer[4].pVolgendBlok->pVolgendBlok->BlokIONummer);
+    EXPECT_EQ(1, baanInfo->BlokPointer[2].pVorigBlok->BlokIONummer);
+    EXPECT_EQ(2, baanInfo->BlokPointer[5].pVorigBlok->BlokIONummer);
+    EXPECT_EQ(0, baanInfo->BlokPointer[3].pVorigBlok->BlokIONummer);
+    EXPECT_EQ(&baanInfo->Blok[2], baanInfo->BlokPointer[3].pVorigBlok->pBlok); // wissel blok moet naar blok 2 wijzen
+    EXPECT_EQ(4, baanInfo->BlokPointer[3].pVorigBlok->pVorigBlok->BlokIONummer);
 
     //
     // bedien de wissel in de andere stand en check de blokken
     //
     EXPECT_CALL(objects->baanMessage, Post(WM_WISSEL_DISPLAY, 0, 0, 0));
-    std::promise<void> done2;
-    {
-        EXPECT_CALL(mHardwareHoog,nieuwItem(_)).WillOnce(Invoke( [&done2] (const hardwareArray_t & data){
-            EXPECT_EQ(100,data.adres);
-            EXPECT_EQ(1,data.data); // get status
-            done2.set_value();
-            return 0;
-        }));
-    }
+    EXPECT_CALL(mHardwareHoog,nieuwItem(_)).WillOnce(Invoke( [] (const hardwareArray_t & data){
+        EXPECT_EQ(100,data.adres);
+        EXPECT_EQ(1,data.data); // get status
+        return 0;
+    }));
 
     EXPECT_EQ(0,baanInfo->IOBits[0]->Aanvraag(13));
     EXPECT_EQ(13,baanInfo->IOBits[0]->Stand);
-    EXPECT_EQ(std::future_status::ready, done2.get_future().wait_for(std::chrono::seconds(1)));
     EXPECT_EQ(2, baanInfo->BlokPointer[1].pVolgendBlok->BlokIONummer);
     EXPECT_EQ(3, baanInfo->BlokPointer[2].pVolgendBlok->BlokIONummer);
     EXPECT_EQ(0, baanInfo->BlokPointer[4].pVolgendBlok->BlokIONummer);
     EXPECT_EQ(&baanInfo->Blok[2], baanInfo->BlokPointer[4].pVolgendBlok->pBlok); // wissel blok moet naar blok 2 wijzen
     EXPECT_EQ(5, baanInfo->BlokPointer[4].pVolgendBlok->pVolgendBlok->BlokIONummer);
+    EXPECT_EQ(1, baanInfo->BlokPointer[2].pVorigBlok->BlokIONummer);
+    EXPECT_EQ(2, baanInfo->BlokPointer[3].pVorigBlok->BlokIONummer);
+    EXPECT_EQ(0, baanInfo->BlokPointer[5].pVorigBlok->BlokIONummer);
+    EXPECT_EQ(&baanInfo->Blok[2], baanInfo->BlokPointer[5].pVorigBlok->pBlok); // wissel blok moet naar blok 2 wijzen
+    EXPECT_EQ(4, baanInfo->BlokPointer[5].pVorigBlok->pVorigBlok->BlokIONummer);
 
 
 }
